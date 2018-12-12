e827f33209991d3e052572b3733c1b6b3d970af9
Purv Chauhan <purvchauhan@gmail.com>
2018-12-12 16:57:34 -0700

    Made tons of changes, forgot to commit! Updates include added headers and footers include semantic UI cdn and custom styling sheet, and many more major and minor changes.

--- app.js
+++ app.js
@@ -21,9 +21,64 @@ var blogSchema = new mongoose.Schema(
 var Blog = mongoose.model("Blog", blogSchema);
 
 //RESTful routes
+
+app.get("/", function(req, res)
+{
+  res.redirect("/blogs");
+});
+
+//INDEX ROUTE
 app.get("/blogs", function(req, res)
 {
-  res.render("index");
+  Blog.find({}, function(err, blogs)
+  {
+    if(err)
+    {
+      console.log(err);
+    }
+    else
+    {
+      res.render("index", {blogs: blogs});
+    }
+  });
+});
+
+//NEW ROUTE
+app.get("/blogs/new", function(req, res)
+{
+  res.render("new");
+});
+
+//CREATE ROUTE
+app.post("/blogs", function(req, res)
+{
+  Blog.create(req.body.blog, function(err, newBlog)
+  {
+    if(err)
+    {
+      res.render("new")
+    }
+    else
+    {
+      res.redirect("/blogs");
+    }
+  })
+});
+
+//SHOW ROUTE
+app.get("/blogs/:id", function(req, res)
+{
+  Blog.findById(req.params.id, function(err, foundBlog)
+  {
+    if(err)
+    {
+      res.redirect("/blogs");
+    }
+    else
+    {
+      res.render("show", {blog: foundBlog});
+    }
+  });
 });
 
 app.listen(8080, 'localhost', function()
