9530ab826e6939ca577f9fb4c72e3e46c3a91e11
Purv Chauhan <purvchauhan@gmail.com>
2018-12-12 15:34:03 -0700

    Added ejs

--- app.js
+++ app.js
@@ -3,10 +3,28 @@ var express = require("express"),
     mongoose = require("mongoose"),
     bodyParser = require("body-parser");
 
+//App Config
 mongoose.connect('mongodb://localhost:27017/blog_app', { useNewUrlParser: true });
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+
+//mongoose model/schema config
+var blogSchema = new mongoose.Schema(
+  {
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now}
+  });
+
+var Blog = mongoose.model("Blog", blogSchema);
+
+//RESTful routes
+app.get("/blogs", function(req, res)
+{
+  res.render("index");
+});
 
 app.listen(8080, 'localhost', function()
 {
