e26014e20a7a3542a2e0392d0e3167b6348de751
Purv Chauhan <purvchauhan@gmail.com>
2018-12-13 00:10:47 -0700

    So engrossed into making the website fully functional and aesthetic, totally forgot about making a commit! Added sanitized text, so no one can put in scripts into the forms. Added PUT and DELETE methods for blogs and many other changes!

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 var express = require("express"),
     app = express(),
     mongoose = require("mongoose"),
-    bodyParser = require("body-parser");
+    bodyParser = require("body-parser"),
+    methodOverride = require("method-override"),
+    expressSanitizer = require("express-sanitizer");
 
 //App Config
 mongoose.connect('mongodb://localhost:27017/blog_app', { useNewUrlParser: true });
+mongoose.set('useFindAndModify', false);
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
+app.use(expressSanitizer());
 
 //mongoose model/schema config
 var blogSchema = new mongoose.Schema(
@@ -52,6 +57,7 @@ app.get("/blogs/new", function(req, res)
 //CREATE ROUTE
 app.post("/blogs", function(req, res)
 {
+  req.body.blog.body = req.sanitize(req.body.blog.body);
   Blog.create(req.body.blog, function(err, newBlog)
   {
     if(err)
@@ -77,6 +83,55 @@ app.get("/blogs/:id", function(req, res)
     else
     {
       res.render("show", {blog: foundBlog});
+    }
+  });
+});
+
+//EDIT ROUTE
+app.get("/blogs/:id/edit", function(req, res)
+{
+  Blog.findById(req.params.id, function(err, foundBlog)
+  {
+    if(err)
+    {
+      res.redirect("/blogs");
+    }
+    else
+    {
+      res.render("edit", {blog: foundBlog});
+    }
+  });
+});
+
+//UPDATE ROUTE
+app.put("/blogs/:id", function(req, res)
+{
+  req.body.blog.body = req.sanitize(req.body.blog.body);
+  Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, udpatedBlog)
+  {
+    if(err)
+    {
+      res.redirect("/blogs");
+    }
+    else
+    {
+      res.redirect("/blogs/" + req.params.id);
+    }
+  });
+});
+
+//DELETE ROUTE
+app.delete("/blogs/:id", function(req, res)
+{
+  Blog.findByIdAndRemove(req.params.id, function(err)
+  {
+    if(err)
+    {
+      res.redirect("/blogs");
+    }
+    else
+    {
+      res.redirect("/blogs");
     }
   });
 });
